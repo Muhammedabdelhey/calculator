68bfc38228a13cdec9860498140d193541797ff8
Muhammedabdelhey <73035247+Muhammedabdelhey@users.noreply.github.com>
2022-11-03 21:10:00 +0200

    delete merge comm

--- src/testcalc/Testcalc.java
+++ src/testcalc/Testcalc.java
@@ -14,14 +14,10 @@ public class Testcalc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("muhammed");
-        System.out.println("merge test last");
-=======
-        System.out.println("Muhammed");
         System.out.println("test merge");
-        //merge
->>>>>>> muhammed
+
+
     }
     
 }
